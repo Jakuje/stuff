c872bcf544521da1d830a2b80445576b588c485a
Jakub Jelen <jjelen@redhat.com>
2017-01-23 08:22:32 +0100

    accept PKCS#11 library as an argument, better checks for errors

--- waittest.c
+++ waittest.c
@@ -1,11 +1,11 @@
 /* compile with
- * gcc -ldl waittest.c
+ * gcc -ldl waittest.c -o waittest
  */
 #include "/home/jjelen/trunk/OpenSC/src/pkcs11/pkcs11.h"
 #include <dlfcn.h>
 #include <stdio.h>
 
-int main() {
+int main(int argc, char **argv) {
 
 	CK_FUNCTION_LIST_PTR fp;
 	CK_RV rv;
@@ -13,10 +13,19 @@ int main() {
 	CK_SLOT_INFO slot_info;
 	int token_present = 0, i;
 	static void *pkcs11_so;
-	char *pkcs11_module = "/usr/lib64/pkcs11/opensc-pkcs11.so";
+	char *pkcs11_module = argv[1];
+
+	if (argc > 1)
+		pkcs11_module = argv[1];
+	else
+		pkcs11_module = "/usr/lib64/pkcs11/opensc-pkcs11.so";
 
 	CK_RV (*C_GetFunctionList)(CK_FUNCTION_LIST_PTR_PTR) = 0;
 	pkcs11_so = dlopen(pkcs11_module, RTLD_NOW);
+	if (!pkcs11_so) {
+		fprintf(stderr, "dlopen(%s) failed", pkcs11_module);
+		return 1;
+	}
 	C_GetFunctionList = (CK_RV (*)(CK_FUNCTION_LIST_PTR_PTR)) dlsym(pkcs11_so, "C_GetFunctionList");
 	rv = C_GetFunctionList(&fp);
 	rv = fp->C_Initialize(NULL_PTR);
@@ -45,4 +54,5 @@ int main() {
 	} while (!token_present);
 	fp->C_Finalize(NULL_PTR);
         dlclose(pkcs11_so);
+	return 0;
 }
